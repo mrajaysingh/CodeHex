6fbcfdbfafc77f6eb5cf4c05d8afb492c84ccca4
Ajay Singh <mrajaysingh8957@gmail.com>
2024-03-08 18:16:44 +0530

    c

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { cx } from "@/src/utils";
 import { Inter, Manrope } from "next/font/google";
 import Header from "@/src/components/Header";
@@ -73,6 +75,8 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
+      <Analytics />
       </body>
     </html>
   );
